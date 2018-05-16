e9cc1fbd1ddfc5df4d4c63208c46b4f15dc8700f
Thiago Faria Rodrigues <thiagorodrigues@email.com>
2018-05-16 20:56:04 -0300

    Update
    Layout Style

--- src/PackageOne/ConnectionScreen.java
+++ src/PackageOne/ConnectionScreen.java
@@ -1,5 +1,7 @@
 package PackageOne;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -7,6 +9,9 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import javax.swing.border.MatteBorder;
 
 import theaterControl.ConectionControl;
 
@@ -14,9 +19,14 @@ import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.font.ShapeGraphicAttribute;
 import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Insets;
+import java.awt.Shape;
+import java.awt.SystemColor;
 
 public class ConnectionScreen {
 
@@ -53,30 +63,48 @@ public class ConnectionScreen {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(255, 255, 255));
+		frame.getContentPane().setForeground(Color.BLACK);
 		frame.setBounds(100, 100, 500, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		
+		
+		
+		
+		
 		JLabel lblUrl = new JLabel("URL:");
+		lblUrl.setFont(new Font("Tahoma", Font.BOLD, 12));
 		
 		JLabel lblUsurio = new JLabel("User:");
+		lblUsurio.setFont(new Font("Tahoma", Font.BOLD, 12));
 		
 		JLabel lblSenha = new JLabel("Pass:");
+		lblSenha.setFont(new Font("Tahoma", Font.BOLD, 12));
 		
 		usertxt = new JTextField();
 		usertxt.setText("ThiagoFar");
 		usertxt.setColumns(10);
 		
 		urltxt = new JTextField();
+		urltxt.setBackground(Color.WHITE);
+		
+	            BevelBorder bevel = new BevelBorder(1);
+				urltxt.setBorder(bevel);
+				usertxt.setBorder(bevel);
+
+		
+	    
 		urltxt.setText("theater.copvvyciryyd.sa-east-1.rds.amazonaws.com/TheaterData");
 		urltxt.setColumns(10);
 		
 		passtxt = new JPasswordField();
 		passtxt.setText("");
 		
-		
+		passtxt.setBorder(bevel);
 		
 		JButton btnConectar = new JButton("Connect");
+		
 		btnConectar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
@@ -100,7 +128,7 @@ public class ConnectionScreen {
 
 			}
 		});
-		
+	
 		JLabel lblTheater = new JLabel("Theater");
 		lblTheater.setFont(new Font("Arial", Font.PLAIN, 30));
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
@@ -118,16 +146,17 @@ public class ConnectionScreen {
 										.addComponent(lblUrl))
 									.addPreferredGap(ComponentPlacement.UNRELATED)
 									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-										.addComponent(urltxt, GroupLayout.DEFAULT_SIZE, 414, Short.MAX_VALUE)
+										.addComponent(urltxt, GroupLayout.PREFERRED_SIZE, 357, GroupLayout.PREFERRED_SIZE)
 										.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
 											.addComponent(passtxt, Alignment.LEADING)
-											.addComponent(usertxt, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(182)
-							.addComponent(btnConectar, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE))
+											.addComponent(usertxt, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))))
+							.addPreferredGap(ComponentPlacement.RELATED, 340, Short.MAX_VALUE))
 						.addGroup(groupLayout.createSequentialGroup()
 							.addGap(189)
-							.addComponent(lblTheater)))
+							.addComponent(lblTheater))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(168)
+							.addComponent(btnConectar, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE)))
 					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
@@ -147,10 +176,11 @@ public class ConnectionScreen {
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblSenha)
 						.addComponent(passtxt, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(82)
+					.addGap(63)
 					.addComponent(btnConectar, GroupLayout.PREFERRED_SIZE, 39, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(94, Short.MAX_VALUE))
+					.addContainerGap(98, Short.MAX_VALUE))
 		);
 		frame.getContentPane().setLayout(groupLayout);
+		
 	}
 }
